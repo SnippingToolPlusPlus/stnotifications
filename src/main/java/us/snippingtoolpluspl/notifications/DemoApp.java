fb29cad914b0a05a73f0e883b9345d598180f591
Talon Daniels <talondaniels@gmail.com>
2014-06-22 07:29:37 -0600

    testing CI build

--- src/main/java/us/snippingtoolpluspl/notifications/DemoApp.java
+++ src/main/java/us/snippingtoolpluspl/notifications/DemoApp.java
@@ -1,10 +1,13 @@
 package us.snippingtoolpluspl.notifications;
 
+import javax.swing.JOptionPane;
+
 public class DemoApp
 {
     public DemoApp()
     {
         System.out.println("World");
+        JOptionPane.showMessageDialog(null, "This was a triumph");
     }
 
     public static void main(String[] args)
