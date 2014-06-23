ae7c2978f7ed40374bd82bd20a8dbf8ce0726b75
ShaneIsrael <shanemisrael@gmail.com>
2014-06-23 17:31:49 -0600

    Fixed conflict issue

--- src/main/java/us/snippingtoolpluspl/notifications/STNotificationWindow.java
+++ src/main/java/us/snippingtoolpluspl/notifications/STNotificationWindow.java
@@ -6,11 +6,5 @@ import javax.swing.JWindow;
  * 
  * @author Shane This is the notifications windows, what you see.
  */
-<<<<<<< HEAD
 public class STNotificationWindow {
-=======
-public class STNotificationWindow extends JWindow
-{
->>>>>>> origin/master
-
 }
